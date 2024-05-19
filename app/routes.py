540cfbb72fea2486846b1f1ffca3bf9c972cb2bb
WiktorStaniszewski <wiktorst.8@gmail.com>
2024-05-19 18:42:02 +0200

    2024-05-13 WS

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,19 @@
 from app import app
-from flask import render_template
+import requests
+from flask import render_template,request,redirect,url_for
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST','GET'])
 def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url = f"https://www.ceneo.pl/{product_id}"
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            return redirect(url_for('product', product_id=product_id))
     return render_template("extract.html")
 
 @app.route('/products')
@@ -18,5 +25,5 @@ def author():
     return render_template("author.html")
 
 @app.route('/product/<product_id>')
-def hello(product_id):
+def product(product_id):
     return render_template("product.html", product_id=product_id)
